bd419a2eb8d1a578aff363b22c8652ef1692e3fe
student <suvo.ece@gmail.com>
2021-05-12 19:14:04 +0000

    First Commit

---
+++ src/main/java/com/caltech/CISpring2/CiSpring2Application.java
@@ -0,0 +1,13 @@
+package com.caltech.CISpring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiSpring2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/CISpring2/CiSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.CISpring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
